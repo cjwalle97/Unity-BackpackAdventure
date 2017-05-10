e8296c837e7069c0912acd4821b5fb1d102c52b9
Trent Butler <redtrentbutler@gmail.com>
2017-05-10 17:45:55 -0500

    create 'BackpackBehaviour'

---
+++ Assets/Scripts/BackpackBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackpackBehaviour : MonoBehaviour {
+
+    private Dictionary<string, List<GameObject>> _backpack;
+
+	// Use this for initialization
+	void Start () {
+        
+        _backpack = new Dictionary<string, List<GameObject>>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
