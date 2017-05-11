43b884380a38754587204a3720aac8fa75cfc82c
cjwalle97 <cjwalle97@gmail.com>
2017-05-11 11:36:12 -0500

    Major Scriptable Asset Update
    Adds the Food, Weapon, and Armor classes. Used themto make Scriptable
    Objects and created a ScriptableObjects folder to place them in

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,11 @@
+﻿namespace Scripts
+{
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "Weapon", menuName = "Item/Permanent/Weapon", 
+        order = 0)]
+    public class Weapon : Item
+    {
+        public int Attack;
+    }
+}
