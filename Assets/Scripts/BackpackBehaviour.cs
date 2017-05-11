008ac6fe6c97b83387f7bb23e466c84fc2aacb93
Trent Butler <redtrentbutler@gmail.com>
2017-05-11 09:38:32 -0500

    add 'AddToBackpack' add 'RemoveFromBackpack'

--- Assets/Scripts/BackpackBehaviour.cs
+++ Assets/Scripts/BackpackBehaviour.cs
@@ -4,16 +4,68 @@ using UnityEngine;
 
 public class BackpackBehaviour : MonoBehaviour {
 
-    private Dictionary<string, List<GameObject>> _backpack;
+    private Dictionary<string, List<Item>> _backpack;	
 
-	// Use this for initialization
-	void Start () {
+    private bool AddToBackpack(Item item)
+    {
+        //CHECK ITEM TYPE
+        //SORT ACCORDLING
+        string itemtypekey = item.Type.ToString();
         
-        _backpack = new Dictionary<string, List<GameObject>>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+        //CHECK IF BACKPACK HAS ITEM ALREADY
+        //IF NOT, ADD NEW LIST FOR THAT ITEM TYPE
+        if (_backpack.ContainsKey(itemtypekey) == false)
+        {
+            List<Item> itemlist = new List<Item>();
+            itemlist.Add(item);
+            _backpack.Add(itemtypekey, itemlist);
+            itemlist = null;
+            return true;
+        }
+
+        //IF BACKPACK HAS ITEM ALREADY, ADD ITEM TO ALREADY EXISTING ITEMLIST
+        _backpack[itemtypekey].Add(item);
+        return false;
+    }
+
+    private bool RemoveFromBackpack(string name)
+    {
+        //CHECK EACH ITEMTYPE IN BACKPACK
+        foreach(var itemtype in _backpack)
+        {
+            //CHECK EACH ITEM IN CATAGORY
+            foreach(var item in itemtype.Value)
+            {
+                //IF MATCH, REMOVE
+                if(item.Name == name)
+                {
+                    itemtype.Value.Remove(item);
+                    return true;
+                }
+            }    
+        }
+        
+        //NO MATCHES RETURN FALSE
+        return false;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        _backpack = new Dictionary<string, List<Item>>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        
+        //  - LOG ALL BACKPACK CONTENTS TO CONSOLE
+        //foreach(var itemtype in _backpack)
+        //{
+        //    foreach(var item in itemtype.Value)
+        //    {
+        //        Debug.Log(itemtype.Key + "(" + item.Name + ")");
+        //    }
+        //}
+        
 	}
 }
