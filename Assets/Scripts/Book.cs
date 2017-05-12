91338d9ff9f433ddb102c5ec7b91bba29cea30ca
cjwalle97 <cjwalle97@gmail.com>
2017-05-12 09:59:47 -0500

    Item Update
    Adds Bomb and Book Scripts. also changes the names of several Prefabs.

---
+++ Assets/Scripts/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace Scripts{
+
+    using UnityEngine;
+
+    [CreateAssetMenu(fileName = "Book", menuName = "Item/Permanent/Book",
+        order = 0)]
+    public class Book : Item {
+        public string SkillName;
+    }
+}
