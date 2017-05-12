74d817257e651c2b7cfc2e4daf56007cc6f7b88e
cjwalle97 <cjwalle97@gmail.com>
2017-05-12 17:48:55 -0500

    Update
    Update

---
+++ Assets/Scripts/Objects/BookofFirebolt.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Book/BookofFirebolt")]
+public class BookofFirebolt : Book {
+    public int FIREDAMAGE;
+}
