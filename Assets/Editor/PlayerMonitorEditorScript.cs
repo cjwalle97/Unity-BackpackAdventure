69d7e8bce491415c3abaa30ce71e8c3a1fc71de0
Trent Butler <redtrentbutler@gmail.com>
2017-05-31 16:35:28 -0500

    finish 'playermonitorscript'

--- Assets/Editor/PlayerMonitorEditorScript.cs
+++ Assets/Editor/PlayerMonitorEditorScript.cs
@@ -11,54 +11,71 @@ using UnityEditor;
 //  - POSITION
 //  - PLAYER HEALTH/MANA
 //MONITOR BACKPACK
-namespace Scripts{
-
-public class PlayerMonitorEditorScript : EditorWindow {
-
-    private Player _player;
-    private Backpack _backpack;
-    private GUIStyle _header = new GUIStyle();
-
-    private void _populate()
+namespace Scripts
+{
+    public class PlayerMonitorEditorScript : EditorWindow
     {
-        _player = GameObject.FindWithTag("Player") as Player;     
-        _backpack = _player.GetComponentInChildren<BackpackBehaviour>().Pack;
-    }
+        private PlayerBehaviour _player;
+        private Transform _playertransform;       
+        private Backpack _backpack;        
+        private GUIStyle _header = new GUIStyle();
+        private static PlayerMonitorEditorScript _window;
 
-    [MenuItem("Tools/LootTableDataBase")]
-    static void Init()
-    {
-        var _window = EditorWindow.GetWindow(typeof(PlayerMonitorEditorScript)) as PlayerMonitorEditorScript;
+        void OnEnable()
+        {
+            Debug.Log("add listener");
+            EditorApplication.playmodeStateChanged += Init;
+        }
+
+        void OnDisable()
+        {
+            Debug.Log("remove listener");
+            EditorApplication.playmodeStateChanged -= Init;
+        }
+
+
+        [MenuItem("Tools/Player Monitor %g")]
+        static void Init()
+        {
+            _window = EditorWindow.GetWindow(typeof(PlayerMonitorEditorScript)) as PlayerMonitorEditorScript;
+            _window._playertransform = GameObject.FindWithTag("Player").transform;
+            _window._player = _window._playertransform.GetComponent<PlayerBehaviour>();
+            _window._backpack = _window._playertransform.GetComponentInChildren<BackpackBehaviour>().Pack;            
+            _window.Show();
+            PlayerMovementBehaviour.OnPlayerMoved.AddListener(_window.Repaint);
+            BackpackBehaviour.OnPackChange.AddListener(_window.Repaint);
+        }
         
-        _window._populate();
-        _window.Show();
+        void OnDestroy()
+        {
+            PlayerMovementBehaviour.OnPlayerMoved.RemoveListener(_window.Repaint);
+        }
+        ////NEEDS WORK
+        private void OnGUI()
+        {
+            string items = "";
+            _header.alignment = TextAnchor.MiddleCenter;
+            _header.fontSize = 20;
+            GUILayout.Space(40);
+            GUILayout.Label("Player Monitor", _header);
+            GUILayout.Space(20);
+            GUILayout.Label(_player.player.Name + " Health(" + _player.player.Health + ")");
+            GUILayout.Space(10);
+            GUILayout.Label(_player.player.Name + " Mana(" + _player.player.Mana + ")");
+            GUILayout.Space(10);
+            GUILayout.Label(_player.player.Name + " Position(" + _playertransform.position + ")");
+
+
+            if(_backpack != null)
+                _backpack.Items.ForEach(x => { items += x.Name + "\n"; });
+            GUILayout.Space(20);
+            GUILayout.Box(items);            
+            
+            _backpack = _playertransform.GetComponentInChildren<BackpackBehaviour>().Pack;
+            _player = _playertransform.GetComponent<PlayerBehaviour>();
+        }
+
+
+
     }
-
-
-    //NEEDS WORK
-    private void OnGUI()
-    {
-        var _window = EditorWindow.GetWindow(typeof(PlayerMonitorEditorScript)) as PlayerMonitorEditorScript;
-        _header.alignment = TextAnchor.MiddleCenter;
-        _header.fontSize = 20;
-        GUILayout.Space(40);
-        GUILayout.Label("Player Monitor", _header);
-        GUILayout.Space(40);
-        GUILayout.Label(_player.Name + " Health(" + _player.Health.ToString() + ")");
-        GUILayout.Space(20);
-        GUILayout.Label(_player.Name + " Mana(" + _player.Mana.ToString() + ")");
-        GUILayout.Space(20);
-        GUILayout.Label(_player.Name + " Position(" + _player.GetComponent<Transform>().position + ")");
-
-
-        string items = "";
-        _backpack.Items.ForEach(x => {items += x.Name + "\n";});
-        GUILayout.Space(20);
-        GUILayout.Box(items);
-        _window._populate();
-    }
-
-    
-
-}
 }
